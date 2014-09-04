b7081cf88ef0557d7d073340b090fe0c38b97965
Thomas van den Berg <paranoid.ndroid@gmail.com>
2014-09-04 15:37:45 +0200

    cleanup

--- src/constants.h
+++ src/constants.h
@@ -3,13 +3,6 @@
 #ifndef SLOW_CONSTANTS_H_
 #define SLOW_CONSTANTS_H_
 
-//const int kCaptureWidth = 640;
-//const int kCaptureHeight = 360;
-const int kCaptureWidth = 1920;
-const int kCaptureHeight = 1080;
-//const int kCaptureWidth = 1280;
-//const int kCaptureHeight = 720;
-
 
 //const int kScreenWidth = 1080;
 //const int kScreenHeight = 480;
@@ -18,8 +11,6 @@ const double kGameSizePadding = 100.0;
 const double kPhysicsScale = 30.0;
 
 const double kFrameRate = 40.0;
-
-const int kGUIWidth = 220;
 
 //const int kScreenWidth = 640;
 //const int kScreenHeight = 480
